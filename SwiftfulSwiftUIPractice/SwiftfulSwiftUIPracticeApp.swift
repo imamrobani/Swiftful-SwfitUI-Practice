9c5aba31be744fb80cfe7c5165fcce4b03c849ec
Imam Robani <imamrob@gmail.com>
2024-04-22 20:38:05 +0700

    Initial Commit

---
+++ SwiftfulSwiftUIPractice/SwiftfulSwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUIPracticeApp.swift
+//  SwiftfulSwiftUIPractice
+//
+//  Created by Imam Robani on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
