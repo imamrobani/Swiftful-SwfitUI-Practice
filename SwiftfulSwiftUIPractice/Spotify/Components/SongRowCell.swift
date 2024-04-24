5188f13e923a042b702ef353c5cc38c67d1c2d3f
Imam Robani <imamrob@gmail.com>
2024-04-24 17:15:29 +0700

    :hammer: add song row cell and create animate header playlist

---
+++ SwiftfulSwiftUIPractice/Spotify/Components/SongRowCell.swift
@@ -0,0 +1,63 @@
+//
+//  SongRowCell.swift
+//  SwiftfulSwiftUIPractice
+//
+//  Created by Imam Robani on 24/04/24.
+//
+
+import SwiftUI
+
+struct SongRowCell: View {
+    
+    var imageSize: CGFloat = 50
+    var imageName: String = Constants.randomImage
+    var title: String = "Some title goes here"
+    var subtitle: String? = "some artist name"
+    var onCellPressed: (() -> Void)? = nil
+    var onEllipsisPressed: (() -> Void)? = nil
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            ImageLoaderView(urlString: imageName)
+                .frame(width: imageSize, height: imageSize)
+                .clipped()
+            
+            VStack(alignment: .leading) {
+                Text(title)
+                    .font(.body)
+                    .fontWeight(.medium)
+                    .foregroundStyle(.spotifyWhite)
+                
+                if let subtitle {
+                    Text(subtitle)
+                        .font(.callout)
+                        .foregroundStyle(.spotifyLightGray)
+                }
+            }
+            .lineLimit(2)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image(systemName: "ellipsis")
+                .font(.subheadline)
+                .foregroundStyle(.spotifyWhite)
+                .padding(16)
+                .background(Color.black.opacity(0.001))
+                .onTapGesture {
+                    onEllipsisPressed?()
+                }
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        
+        VStack {
+            SongRowCell()
+            SongRowCell()
+            SongRowCell()
+            SongRowCell()
+        }
+    }
+}
--- SwiftfulSwiftUIPractice/Spotify/Core/SpotifyPlaylistView.swift
+++ SwiftfulSwiftUIPractice/Spotify/Core/SpotifyPlaylistView.swift
@@ -12,6 +12,9 @@ struct SpotifyPlaylistView: View {
     var product: Product = .mock
     var user: User = .mock
     
+    @State private var products: [Product] = []
+    @State private var showHeader: Bool = false
+    
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
@@ -24,6 +27,9 @@ struct SpotifyPlaylistView: View {
                         subtitle: product.description,
                         imageName: product.thumbnail
                     )
+                    .readingFrame { frame in
+                        showHeader = frame.maxY < 150
+                    }
                     
                     PlaylistDescriptionCell(
                         desc: product.description,
@@ -37,8 +43,62 @@ struct SpotifyPlaylistView: View {
                         onPlayPressed: nil
                     )
                     .padding(.horizontal, 16)
+                    
+                    ForEach(products) { product in
+                        SongRowCell(
+                            imageSize: 50,
+                            imageName: product.firstImage,
+                            title: product.title,
+                            subtitle: product.brand,
+                            onCellPressed: {
+                                
+                            },
+                            onEllipsisPressed: {
+                                
+                            }
+                        )
+                            .padding(.leading, 16)
+                    }
                 }
             }
+            .scrollIndicators(.hidden)
+            
+            ZStack {
+                Text(product.title)
+                    .font(.headline)
+                    .padding(.vertical, 20)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.spotifyBlack)
+                    .offset(y: showHeader ? 0 : -40)
+                    .opacity(showHeader ? 1 : 0)
+                
+                Image(systemName: "chevron.left")
+                    .font(.title3)
+                    .padding(10)
+                    .background(showHeader ? Color.black.opacity(0.001) : Color.spotifyGray.opacity(0.7))
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .onTapGesture {
+                        
+                    }
+                    .padding(.leading, 16)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .foregroundStyle(.spotifyWhite)
+            .animation(.smooth(duration: 0.2), value: showHeader)
+            .frame(maxHeight: .infinity, alignment: .top)
+            
+        }
+        .task {
+            await getData()
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+    
+    private func getData() async {
+        do {
+            products = try await DatabaseHelper().getProducts()
+        } catch {
+            
         }
     }
 }
