02855d0f2976a03836f009aa8d3b41480a328471
Imam Robani <imamrob@gmail.com>
2024-05-15 07:33:08 +0700

    :hammer: add header and filter netflix home view

---
+++ SwiftfulSwiftUIPractice/Netflix/Components/NetflixFilterCell.swift
@@ -0,0 +1,50 @@
+//
+//  NetflixFilterCell.swift
+//  SwiftfulSwiftUIPractice
+//
+//  Created by Imam Robani on 15/05/24.
+//
+
+import SwiftUI
+
+struct NetflixFilterCell: View {
+    
+    var title: String = "Categories"
+    var isDropDown: Bool = true
+    var isSelected: Bool = false
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            Text(title)
+            
+            if isDropDown {
+                Image(systemName: "chevron.down")
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
+        .background(
+            ZStack {
+                Capsule(style: .circular)
+                    .fill(.netflixDarkGray)
+                    .opacity(isSelected ? 1 : 0)
+                
+                Capsule(style: .circular)
+                    .stroke(lineWidth: 1)
+            }
+        )
+        .foregroundStyle(.netflixLightGray)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        
+        VStack {
+            NetflixFilterCell()
+            NetflixFilterCell(isSelected: true)
+            NetflixFilterCell(isDropDown: false)
+        }
+    }
+}
